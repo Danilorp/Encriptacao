bdb89be0b81da80a5961f21c7c5224f2abdc27c5
Danilo Pinha <danilorpinha@hotmail.com>
2020-10-26 21:20:48 -0300

    Add project files.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,79 @@
+﻿using Encriptacao.Context;
+using Encriptacao.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Encriptacao.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        private Contexto db = new Contexto();
+        private static string AesIV256BD = @"%j?TmFP6$BbMnY$@";//16 caracteres
+        private static string AesKey256BD = @"rxmBUJy]&,;3jKwDTzf(cui$<nc2EQr)";//32 caracteres
+
+
+        #region Index
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View(db.Usuarios.ToList());
+        }
+        #endregion
+
+        #region Create - GET
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Create - POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Models.UsuarioModel usuarioModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Hash de Senha
+                usuarioModel.Senha = BCrypt.Net.BCrypt.HashPassword(usuarioModel.Senha);
+                usuarioModel.ConfirmeSenha = usuarioModel.Senha;
+
+                //AesCryptoServiceProvider
+                AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                aes.BlockSize = 128;
+                aes.KeySize = 256;
+                aes.IV = Encoding.UTF8.GetBytes(AesIV256BD);
+                aes.Key = Encoding.UTF8.GetBytes(AesKey256BD);
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                // Convertendo string para byte array
+                byte[] src = Encoding.Unicode.GetBytes(usuarioModel.Email);
+
+                //Encriptação
+                using (ICryptoTransform encrypt = aes.CreateEncryptor())
+                {
+                    byte[] dest = encrypt.TransformFinalBlock(src, 0, src.Length);
+
+                    // Converte byte array para string de base 64
+                    usuarioModel.Email = Convert.ToBase64String(dest);
+                }
+                db.Usuarios.Add(usuarioModel);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuarioModel);
+        }
+        #endregion
+
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Encriptacao.Models
+{
+    public class UsuarioModel
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do usuario")]
+
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o Email para Login")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a Senha")]
+        [DataType(DataType.Password)]
+        [StringLength(255, MinimumLength = 3,
+            ErrorMessage = "Senha minima 3 Caracteres")]
+
+        public String Senha { get; set; }
+
+        //[NotMapped]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Senha), ErrorMessage ="Senha não Confere")]
+
+        public String ConfirmeSenha { get; set; }
+
+        [Required(ErrorMessage ="informe o Nível")]
+
+        public String Nivel { get; set; }
+
+
+    }
+}
